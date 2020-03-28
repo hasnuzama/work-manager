76394c2ef130ec2eac403d20d3bfb1fa1fedcff8
Hasnu zama <hasnuzama@gmail.com>
2020-03-28 11:24:40 +0530

    Added missing files

---
+++ WorkPlanner/Helpers/SecurityHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace WorkPlanner.Helpers
+{
+	public class SecurityHelper
+	{
+		public static string CalculatePasswordHash(string input)
+		{
+			input = input ?? string.Empty;
+			// step 1, calculate MD5 hash from input
+			MD5 md5 = System.Security.Cryptography.MD5.Create();
+			byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+			byte[] hash = md5.ComputeHash(inputBytes);
+
+			// step 2, convert byte array to hex string
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < hash.Length; i++)
+			{
+				sb.Append(hash[i].ToString("X2"));
+			}
+			return sb.ToString();
+		}
+	}
+}
